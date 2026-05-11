d8b38203d9299e6a0963fa942995409318ec3263
tiss-asleep <jambalm0674@gmail.com>
2026-05-11 14:57:46 -0400

    hltv_scraper:create_driver commited

---
+++ scraper/hltv_scraper.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def create_driver(headless: bool = False) -> webdriver.Chrome:
+    options = Options()
+
+    # if headless, Chrome runs in the background, no window opens
+    if headless:
+        options.add_argument("--headless=new")
+
+    # to prevent common issues on macOS/linux
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    # spoof a User-Agent
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    )
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    return driver
